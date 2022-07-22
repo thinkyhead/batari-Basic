e84f825dc225fd66587dd8791d999ecfba17169b
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-07-22 14:23:31 -0500

    Update install_ux.sh for Darwin ZSH

--- install_ux.sh
+++ install_ux.sh
@@ -1,33 +1,42 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 OS=$(uname -s)
 if [ ! "$OS" = Linux -a ! "$OS" = Darwin ] ; then
-  echo "##### WARNING: Unsupport \"$OS\" OS. You may need to create"
+  echo "##### WARNING: Unsupported \"$OS\" OS. You may need to create"
   echo "#####          your own bB binaries."
 fi
 
-if [ "$(uname -s)" = Linux ] ; then
-	PROFILE=~/.bashrc
+if [ "$(uname -s)" = Darwin ] ; then
+
+  PROFILE=~/.zprofile
+  #[ -r ~/.zshrc ] && PROFILE=~/.zshrc
+
 else
-	PROFILE=~/.profile
+
+  if [ "$(uname -s)" = Linux ] ; then
+    PROFILE=~/.bashrc
+  else
+    PROFILE=~/.profile
+  fi
+
+  # Need to test for these, because if they exist than bash won't read .profile
+  for PRO in ~/.bash_profile ~/.bash_login ~/.bashrc ; do
+    [ -r "$PRO" ] && PROFILE="$PRO"
+  done
+
 fi
 
-# need to test for these, because if they exist than bash won't read .profile
-for PRO in ~/.bash_profile ~/.bash_login ~/.bashrc ; do
-  [ -r "$PRO" ] && PROFILE="$PRO"
-done
-
-bB=$PWD
+bB="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"
 
 cat <<EOF
 
 __________________________The_bB_Unix_Installer_v1__________________________
 
-This script will update your $PROFILE file to 
+This script will update your $PROFILE file to
 set the following variables each time you open a terminal window.
 
   export bB="$bB"
-  export PATH=\$PATH:\$bB
+  export PATH="\$bB:\$PATH"
 
 You may run this script as many times as you like, and should do so if you're
 installing a new version of bB, or if you relocate this bB directory.
@@ -38,18 +47,18 @@ EOF
 read ANSWER
 [ "$ANSWER" ]  && exit 1
 
-# ensure the profile exists
+# Ensure the profile exists
 [ -r "$PROFILE" ] || touch "$PROFILE"
 
-# create a backup of the profile...
+# Create a backup of the profile...
 cp "$PROFILE" "$PROFILE.$(date +%y%m%d%H%M%S)"
 
-# remove any old bB entries 
+# Remove any old bB entries
 grep -v bB "$PROFILE" > "$PROFILE.new"
 
 echo "##### bB variables, added by installer on $(date +%y/%m/%d)" >> "$PROFILE.new"
 echo "export bB=\"$bB\"" >> "$PROFILE.new"
-echo 'export PATH=$PATH:$bB' >> "$PROFILE.new"
+echo 'export PATH="$bB:$PATH"' >> "$PROFILE.new"
 
 if [ ! -r "$PROFILE.new" ] ; then
   echo
@@ -58,7 +67,7 @@ if [ ! -r "$PROFILE.new" ] ; then
   exit 2
 fi
 
-# move the contents instead, to preserve any custom permissions on the profile
+# Move the contents instead, to preserve any custom permissions on the profile
 cat "$PROFILE.new" > "$PROFILE" && rm "$PROFILE.new"
 
 cat <<EOF
@@ -66,13 +75,12 @@ $PROFILE has been updated successfully.
 
 To test the new setup...
 
-  1. open another terminal window. 
+  1. open another terminal window.
      (the bB and PATH variables will now be active in any new terminal window)
   2. type:  cd "\$bB/samples"
   3. type:  2600basic.sh zombie_chase.bas
 
-This should create a zombie_chase.bas.bin binary file in the samples directory 
+This should create a zombie_chase.bas.bin binary file in the samples directory
 that will work on real hardware, in stella, or any other emulator.
 
 EOF
-
